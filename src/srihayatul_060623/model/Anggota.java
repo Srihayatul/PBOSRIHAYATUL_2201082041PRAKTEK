63e2c84f58bbf8291913bb32d6752d853730d3e9
Srihayatul <126839876+Srihayatul@users.noreply.github.com>
2023-06-19 23:00:20 +0700

    Update Anggota.java

--- src/srihayatul_060623/model/Anggota.java
+++ src/srihayatul_060623/model/Anggota.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package annisa_060623.model;
+package srihayatul_060623.model;
 
 /**
  *
